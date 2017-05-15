11e98a0e90db118d1e3b3032b9bd1905444bfd17
Josh Peterson <petersonjm1@gmail.com>
2017-05-15 06:52:01 -0400

    Add a few more validations for includes

--- app/services/validate_input.rb
+++ app/services/validate_input.rb
@@ -1,11 +1,18 @@
 class ValidateInput
-  @relative_include_message = "We cannot compute the layout for files with \
-relative include paths. Not to worry though! You can use relative include \
-paths by running layout on your machine. Download it \
-<a href=\"https://github.com/joshpeterson/layout\">here</a>."
+  @@relative_include_message = "We can only compute the layout for files with \
+system include paths. Not to worry though! You can run layout on your machine. \
+Download it <a href=\"https://github.com/joshpeterson/layout\">here</a>."
+
+  @@relative_include_regex = /#include\s*"/
+  @@relative_path_in_system_include_regex = /#include\s*<.*\.\./
+  @@absolute_path_system_include_regex = %r{#include\s*<.*\/}
 
   def self.call(input)
-    return Result.failed(@relative_include_message) if /#include\s*"/ =~ input
+    if @@relative_include_regex =~ input ||
+       @@relative_path_in_system_include_regex =~ input ||
+       @@absolute_path_system_include_regex =~ input
+      return Result.failed(@@relative_include_message)
+    end
 
     Result.successful
   end
--- test/services/validate_input_test.rb
+++ test/services/validate_input_test.rb
@@ -1,24 +1,30 @@
 require 'test_helper'
 
 class ValidateInputTest < MiniTest::Test
-  def test_validation_of_input
-    test_data = [{ input: 'struct Test { int i; };',
+  @@relative_path_message = "We can only compute the layout for files with system \
+include paths. Not to worry though! You can run layout on your machine. Download \
+it <a href=\"https://github.com/joshpeterson/layout\">here</a>."
+  @@test_data = [{ input: 'struct Test { int i; };',
                    result: true },
                  { input: '#include  "foo.h" struct Test { int i; };',
-                   result: false, error_message: "We cannot compute the layout \
-for files with relative include paths. Not to worry though! You can use relative \
-include paths by running layout on your machine. Download it \
-<a href=\"https://github.com/joshpeterson/layout\">here</a>." }]
+                   result: false, error_message: @@relative_path_message },
+                 { input: '#include <path/../foo.h> struct Test { int i; };',
+                   result: false, error_message: @@relative_path_message },
+                 { input: '#include </path/to/foo.h> struct Test { int i; };',
+                   result: false, error_message: @@relative_path_message }]
 
-    test_data.each { |data| validate_input(data) }
+  def test_validation_of_input
+    @@test_data.each { |data| validate_input(data) }
   end
 
   def validate_input(data)
     result = ValidateInput.call(data[:input])
-    assert_equal(data[:result], result.success)
+    assert_equal(data[:result], result.success,
+                 "The input '#{data[:input]}' validated, which is not expected.")
 
     return if data[:error_message].nil?
 
-    assert_equal(data[:error_message], result.message)
+    assert_equal(data[:error_message], result.message,
+                 "The error message for input '#{data[:input]}' is not correct.")
   end
 end
