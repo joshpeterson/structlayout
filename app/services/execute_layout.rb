2815f96cc95aba4474dba5b1c00b4eb8c874b097
Josh Peterson <petersonjm1@gmail.com>
2017-05-23 06:59:25 -0400

    Handle errors in the layout output.

--- app/services/execute_layout.rb
+++ app/services/execute_layout.rb
@@ -1,5 +1,19 @@
+require 'open3'
+
 class ExecuteLayout
   def self.call(path)
-    `./cde-package/layout.cde #{path}`
+    Dir.chdir('cde-package/cde-root/home/josh/layout') do
+      stdout, stderr, status = Open3.capture3("./layout.cde #{path}")
+      return strip_filenames(stderr) if status != 0
+      return stdout
+    end
+  end
+
+  def self.strip_filenames(stderr)
+    stripped = ''
+    stderr.each_line do |line|
+      stripped += line.partition(':')[2]
+    end
+    stripped
   end
 end
