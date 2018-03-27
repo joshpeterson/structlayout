01d7ed690070d67d7ab9fab4d0966f9210c36de3
Josh Peterson <petersonjm1@gmail.com>
2018-03-27 06:53:22 -0400

    Add URL parameters for type and compiler arguments

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,9 @@
 class WelcomeController < ApplicationController
   def index
     @initial_code = params[:code] if params[:code].present?
+    @initial_args = params[:args] if params[:args].present?
+    type = params[:type] if params[:type].present?
+    @c_is_enabled = type == 'c'
+    @cpp_is_enabled = !@c_is_enabled
   end
 end
