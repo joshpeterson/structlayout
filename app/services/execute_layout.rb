d8b7b41af45bc832f6a323697b08f1731db88b75
Josh Peterson <petersonjm1@gmail.com>
2020-01-08 12:04:14 +0000

    Attempt to use -nostdlib to locate headers

--- app/services/execute_layout.rb
+++ app/services/execute_layout.rb
@@ -3,11 +3,17 @@
 require 'open3'
 
 class ExecuteLayout
+  @@includes = '-Iusr/lib/llvm-9.0/lib/clang/9.0.1/include '\
+               '-Iusr/include/c++/7.4.0 '\
+               '-Iusr/include/x86_64-linux-gnu/c++/7 '\
+               '-Iusr/include/c++/7.4.0/backward '\
+               '-Iusr/include/x86_64-linux-gnu '\
+               '-Iusr/include'
   def self.call(path, flags)
     Dir.chdir('layout-package') do
       stdout, stderr, status = Open3.capture3(
         { 'LD_LIBRARY_PATH' => '.' },
-        "./layout #{path} -isystem usr/include #{flags}"
+        "./layout #{path} -nostdlib #{@@includes} #{flags}"
       )
       return stdout, strip_filenames(stderr), status
     end
